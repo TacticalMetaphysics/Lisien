346ae3f0f9dea71ed391a85e33cb4fa3760123c4
Zachary Spector <public@zacharyspector.com>
2024-10-07 08:54:59 +1300

    Implement "rulebooks_dump" and change "universal" back to "universals"

--- lisien/lisien/query.py
+++ lisien/lisien/query.py
@@ -1165,7 +1165,7 @@ class ParquetDBHolder:
 			("turn", pa.uint64()),
 			("tick", pa.uint64()),
 		],
-		"universal": [
+		"universals": [
 			("key", pa.binary()),
 			("branch", pa.string()),
 			("turn", pa.uint64()),
@@ -1571,7 +1571,10 @@ class ParquetDBHolder:
 			)
 
 	def universals_dump(self):
-		return self._db.read(dataset_name="universal").to_pylist()
+		return self._db.read(dataset_name="universals").to_pylist()
+
+	def rulebooks_dump(self):
+		return self._db.read(dataset_name="rulebooks").to_pylist()
 
 	@staticmethod
 	def echo(it):
