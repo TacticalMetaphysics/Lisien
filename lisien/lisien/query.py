daf0f5d9d5041d8cc9f3835e8a5a196003fba2a7
Zachary Spector <public@zacharyspector.com>
2024-10-08 10:08:25 +1300

    Fix rule dumps
    Former-commit-id: 087494686b0478cbf2c8c61952a866f620f94e55

--- lisien/lisien/query.py
+++ lisien/lisien/query.py
@@ -3339,7 +3339,7 @@ class ParquetQueryEngine(AbstractLiSEQueryEngine):
 				d["branch"],
 				d["turn"],
 				d["tick"],
-				unpack(d["typ"]),
+				unpack(d[typ]),
 			)
 
 	def rule_triggers_dump(
