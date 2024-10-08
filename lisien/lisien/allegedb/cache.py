56780b1b1804ac1fdda01f3803d97914c767bb95
Zachary Spector <public@zacharyspector.com>
2024-10-08 13:32:53 +1300

    Handle sorting various incoming data types

--- lisien/lisien/allegedb/cache.py
+++ lisien/lisien/allegedb/cache.py
@@ -413,6 +413,14 @@ class Cache:
 		each branch is chronological of itself.
 
 		"""
+
+		def sort_key(v):
+			if isinstance(v, tuple):
+				return (2,) + tuple(map(repr, v))
+			if isinstance(v, str):
+				return 1, v
+			return 0, repr(v)
+
 		branches = defaultdict(list)
 		for row in data:
 			branches[row[-4]].append(row)
@@ -425,7 +433,7 @@ class Cache:
 		store = self.store
 		while branch2do:
 			branch = branch2do.popleft()
-			for row in sorted(branches[branch]):
+			for row in sorted(branches[branch], key=sort_key):
 				store(*row, planning=False, loading=True)
 			if branch in childbranch:
 				branch2do.extend(childbranch[branch])
