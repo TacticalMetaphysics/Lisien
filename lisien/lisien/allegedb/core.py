fbc5b93d78c87d940bb8134485052aa00c2778e6
Zachary Spector <public@zacharyspector.com>
2024-10-08 18:52:15 +1300

    Make edges keyframes tidier

--- lisien/lisien/allegedb/core.py
+++ lisien/lisien/allegedb/core.py
@@ -1568,8 +1568,12 @@ class ORM:
 							else:
 								if dest in ekg[orig]:
 									del ekg[orig][dest]
-								if orig in evkg and dest in evkg[orig]:
+								if not ekg[orig]:
+									del ekg[orig]
+							if orig in evkg and dest in evkg[orig]:
 									del evkg[orig][dest]
+								if not evkg[orig]:
+									del evkg[orig]
 						elif exists:
 							ekg[orig] = {dest: exists}
 			if graph in edges_keyframe:
