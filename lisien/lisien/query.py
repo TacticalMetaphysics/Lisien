3963c79fa34f8791f25dbdf536854f1d30693ac4
Zachary Spector <public@zacharyspector.com>
2024-10-08 13:56:48 +1300

    Fix `edges_dump` again
    Former-commit-id: b8573f7a190020b7f30e2e654e3fc5ded758cea6

--- lisien/lisien/query.py
+++ lisien/lisien/query.py
@@ -4585,6 +4585,7 @@ class ParquetQueryEngine(AbstractLiSEQueryEngine):
 				unpack(d["graph"]),
 				unpack(d["orig"]),
 				unpack(d["dest"]),
+				d["idx"],
 				d["branch"],
 				d["turn"],
 				d["tick"],
