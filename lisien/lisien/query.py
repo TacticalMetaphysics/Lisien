5e3f67227492df7c46de190ec04c35f6d5e90bd0
Zachary Spector <public@zacharyspector.com>
2024-10-12 02:29:54 +1300

    Don't count handledness records toward keyframe

--- lisien/lisien/query.py
+++ lisien/lisien/query.py
@@ -3420,7 +3420,6 @@ class ParquetQueryEngine(AbstractLiSEQueryEngine):
 				("_portal_rules_handled", "portal_rules_handled"),
 			]:
 				if getattr(self, attr):
-					records += len(getattr(self, attr))
 					put(
 						(
 							"silent",
