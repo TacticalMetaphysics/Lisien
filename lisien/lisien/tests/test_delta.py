3acadb0b0b8c8304735ee1d725839e5392f0f66a
Zachary Spector <public@zacharyspector.com>
2025-02-14 17:07:39 +1300

    Test character existence delta
    Indeed, they're not being reported right

--- lisien/lisien/tests/test_delta.py
+++ lisien/lisien/tests/test_delta.py
@@ -1,8 +1,30 @@
-from ..facade import EngineFacade
+import pytest
 
 
-def test_character_existence_delta():
-	pass
+@pytest.mark.parametrize("slow", [0, 1, 2])
+def test_character_existence_delta(serial_engine, slow):
+	eng = serial_engine
+	eng.add_character(1)
+	eng.add_character(2)
+	eng.next_turn()
+	eng.add_character(3)
+	if slow == 2:
+		eng.branch = "branch"
+	elif slow == 1:
+		eng.next_turn()
+		eng.next_turn()
+	else:
+		eng.next_turn()
+	del eng.character[2]
+	eng.add_character(4)
+	delta0 = eng.get_delta(("trunk", 0), ("trunk", 1))
+	assert 3 in delta0
+	assert 2 not in delta0
+	delta1 = eng.get_delta(
+		("trunk", 1), ("branch", 1) if slow else ("trunk", 2)
+	)
+	assert 2 in delta1
+	assert delta1[2] is None
 
 
 def test_unit_delta():
