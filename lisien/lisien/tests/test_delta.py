6ab637af1ad7915f2905ec59e268c33921121e52
Zachary Spector <public@zacharyspector.com>
2025-02-14 16:07:40 +1300

    Stub out delta tests

---
+++ lisien/lisien/tests/test_delta.py
@@ -0,0 +1,57 @@
+from ..facade import EngineFacade
+
+
+def test_character_existence_delta():
+	pass
+
+
+def test_unit_delta():
+	pass
+
+
+def test_character_stat_delta():
+	pass
+
+
+def test_node_existence_delta():
+	pass
+
+
+def test_node_stat_delta():
+	pass
+
+
+def test_portal_existence_delta():
+	pass
+
+
+def test_thing_location_delta():
+	pass
+
+
+def test_character_rulebook_delta():
+	pass
+
+
+def test_unit_rulebook_delta():
+	pass
+
+
+def test_character_thing_rulebook_delta():
+	pass
+
+
+def test_character_place_rulebook_delta():
+	pass
+
+
+def test_character_portal_rulebook_delta():
+	pass
+
+
+def test_node_rulebook_delta():
+	pass
+
+
+def test_portal_rulebook_delta():
+	pass
