TacticalMetaphysics/LiSE

Correct signature of `get_keyframe`

LiSE/LiSE/allegedb/query.py
@@ -300,7 +300,9 @@ class AbstractQueryEngine:
 		pass
 
 	@abstractmethod
-	def get_keyframe(self) -> Tuple[list, list, list]:
+	def get_keyframe(
+		self, graph: Key, branch: str, turn: int, tick: int
+	) -> Tuple[list, list, list]:
 		pass
 
 	@abstractmethod
@@ -764,7 +766,7 @@ class QueryEngine(AbstractQueryEngine):
 		for graph, branch, turn, tick in self.call_one("keyframes_list"):
 			yield unpack(graph), branch, turn, tick
 
-	def get_keyframe(self, graph, branch, turn, tick):
+	def get_keyframe(self, graph: Key, branch: str, turn: int, tick: int):
 		unpack = self.unpack
 		stuff = self.call_one(
 			"get_keyframe", self.pack(graph), branch, turn, tick

TacticalMetaphysics/Lisien

Former-commit-id: 723ffcf282d33cc8c3eacb080239534ac8f6271d

lisien/lisien/allegedb/query.py
@@ -300,7 +300,9 @@ class AbstractQueryEngine:
 		pass
 
 	@abstractmethod
-	def get_keyframe(self) -> Tuple[list, list, list]:
+	def get_keyframe(
+		self, graph: Key, branch: str, turn: int, tick: int
+	) -> Tuple[list, list, list]:
 		pass
 
 	@abstractmethod
@@ -718,7 +720,7 @@ class QueryEngine(AbstractQueryEngine):
 		):
 			yield unpack(graph), branch, turn, tick
 
-	def get_keyframe_graph(self, graph, branch, turn, tick):
+	def get_keyframe_graph(self, graph: Key, branch: str, turn: int, tick: int):
 		unpack = self.unpack
 		stuff = self.call_one(
 			"get_keyframe_graph", self.pack(graph), branch, turn, tick