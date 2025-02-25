0666421fce9d630bca3f4f55ea1fea1852dd6f51
Zachary Spector <public@zacharyspector.com>
2025-02-25 18:44:26 +1300

    Fix serial run of `next_turn`
    Former-commit-id: 13ada4e6bdd985d2a98a65c5bc057c38a861dafc

--- lisien/lisien/engine.py
+++ lisien/lisien/engine.py
@@ -213,9 +213,10 @@ class NextTurn(Signal):
 			)
 			engine.turn += 1
 		now = engine._btt()
-		for i, last_updated in enumerate(engine._worker_updated_btts):
-			if last_updated != now:
-				engine._update_worker_process_state(i)
+		if hasattr(engine, "_worker_updated_btts"):
+			for i, last_updated in enumerate(engine._worker_updated_btts):
+				if last_updated != now:
+					engine._update_worker_process_state(i)
 		results = []
 		if hasattr(engine, "_rules_iter"):
 			it = engine._rules_iter
