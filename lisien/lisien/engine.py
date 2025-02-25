7bbdc32ffdbfb01bd218de4f59205c88af8ce3a7
Zachary Spector <public@zacharyspector.com>
2025-02-25 18:46:12 +1300

    Reuse messages to worker processes in `next_turn`

--- lisien/lisien/engine.py
+++ lisien/lisien/engine.py
@@ -212,11 +212,8 @@ class NextTurn(Signal):
 				0,
 			)
 			engine.turn += 1
-		now = engine._btt()
 		if hasattr(engine, "_worker_updated_btts"):
-			for i, last_updated in enumerate(engine._worker_updated_btts):
-				if last_updated != now:
-					engine._update_worker_process_state(i)
+			engine._update_all_worker_process_states()
 		results = []
 		if hasattr(engine, "_rules_iter"):
 			it = engine._rules_iter
