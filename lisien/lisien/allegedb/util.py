403891f3aaa1ae47c9c91006cf8618fbc09f655f
Zachary Spector <public@zacharyspector.com>
2025-03-01 14:59:19 +1300

    Move `_garbage_ctx` and `_garbage_dec` out of `garbage`
    Because my highlighter was having trouble with them that way. Whatever...
    
    
    Former-commit-id: 7ed8293bc1b6dee32837f855450b7ae7c3cb2e0d

--- lisien/lisien/allegedb/util.py
+++ lisien/lisien/allegedb/util.py
@@ -3,6 +3,38 @@ from contextlib import contextmanager
 from functools import wraps
 
 
+@contextmanager
+def _garbage_ctx(collect=True):
+	"""Context manager to disable the garbage collector
+
+	:param collect: Whether to immediately collect garbage upon context exit
+
+	"""
+	gc_was_active = gc.isenabled()
+	if gc_was_active:
+		gc.disable()
+	yield
+	if gc_was_active:
+		gc.enable()
+	if collect:
+		gc.collect()
+
+
+def _garbage_dec(fn: callable, collect=True) -> callable:
+	"""Decorator to disable the garbage collector for a function
+
+	:param collect: Whether to immediately collect garbage when the function returns
+
+	"""
+
+	@wraps(fn)
+	def garbage(*args, **kwargs):
+		with _garbage_ctx(collect=collect):
+			return fn(*args, **kwargs)
+
+	return garbage
+
+
 def garbage(arg: callable = None, collect=True):
 	"""Disable the garbage collector, then re-enable it when done.
 
@@ -13,40 +45,10 @@ def garbage(arg: callable = None, collect=True):
 
 	"""
 
-	@contextmanager
-	def garbage_ctx(collect=True):
-		"""Context manager to disable the garbage collector
-
-		:param collect: Whether to immediately collect garbage upon context exit
-
-		"""
-		gc_was_active = gc.isenabled()
-		if gc_was_active:
-			gc.disable()
-		yield
-		if gc_was_active:
-			gc.enable()
-		if collect:
-			gc.collect()
-
-	def garbage_dec(fn: callable, collect=True) -> callable:
-		"""Decorator to disable the garbage collector for a function
-
-		:param collect: Whether to immediately collect garbage when the function returns
-
-		"""
-
-        @wraps(fn)
-		def garbage(*args, **kwargs):
-			with garbage_ctx(collect=collect):
-				return fn(*args, **kwargs)
-
-		return garbage
-
 	if arg is None:
-		return garbage_ctx(collect=collect)
+		return _garbage_ctx(collect=collect)
 	else:
-		return garbage_dec(arg, collect=collect)
+		return _garbage_dec(arg, collect=collect)
 
 
 def world_locked(fn: callable) -> callable:
