4ecbb18565d549612f835714d467f8e9d37a4285
Zachary Spector <public@zacharyspector.com>
2025-03-12 15:25:15 +1300

    Fix another import
    Former-commit-id: 849b3b0ab5001e2fcc9b5a2bddc39f63686a02d1

--- lisien/lisien/node.py
+++ lisien/lisien/node.py
@@ -24,10 +24,9 @@ from typing import Iterator, List, Optional, Union
 
 from networkx import shortest_path, shortest_path_length
 
-from . import rule
-from .allegedb import Key, graph
-from .allegedb.window import HistoricKeyError
-from .exc import AmbiguousUserError
+from . import rule, graph
+from .typing import Key
+from .exc import AmbiguousUserError, HistoricKeyError
 from .facade import FacadePlace, FacadeThing
 from .query import StatusAlias
 from .util import AbstractCharacter, AbstractThing, getatt
