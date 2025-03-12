8a1ad27617161632df6c71821a0c70d295d48135
Zachary Spector <public@zacharyspector.com>
2025-03-12 16:22:17 +1300

    Update `test_branch_plan` for the refactor
    Former-commit-id: cfe75a2911acbb44ea7ae350815a56ce57763cbc

--- lisien/lisien/tests/test_branch_plan.py
+++ lisien/lisien/tests/test_branch_plan.py
@@ -1,62 +1,56 @@
-import pytest
-
-from lisien import allegedb
-
-
-@pytest.fixture(scope="function")
-def orm():
-	with allegedb.ORM("sqlite:///:memory:") as it:
-		yield it
-
-
-def test_single_plan(orm):
-	g = orm.new_digraph("graph")
+def test_single_plan(engy):
+	assert engy.turn == 0
+	g = engy.new_digraph("graph")
 	g.add_node(0)
-	orm.turn = 1
+	engy.next_turn()
+	assert engy.turn == 1
 	g.add_node(1)
-	with orm.plan():
-		orm.turn = 2
+	with engy.plan():
+		engy.turn = 2
 		g.add_node(2)
 		g.node[2]["clever"] = False
-		orm.turn = 3
+		engy.turn = 3
 		g.node[2]["funny"] = True
 		g.add_node(3)
-		orm.turn = 4
+		engy.turn = 4
 		g.node[2]["successful"] = True
-	orm.turn = 1
+	engy.turn = 1
 	assert 2 not in g.node
-	orm.branch = "b"
+	engy.branch = "b"
 	assert 2 not in g.node
 	assert 1 in g
-	orm.turn = 2
+	engy.next_turn()
+	assert engy.turn == 2
 	assert 2 in g.node
 	assert g.node[2].keys() == {"clever"}
-	orm.turn = 3
+	engy.next_turn()
+	assert engy.turn == 3
 	assert g.node[2]["funny"]
 	assert 3 in g
 	assert g.node[2].keys() == {"funny", "clever"}
-	orm.turn = 4
+	engy.next_turn()
+	assert engy.turn == 4
 	assert g.node[2].keys() == {"funny", "clever", "successful"}
-	orm.turn = 2
+	engy.turn = 2
 	assert g.node[2].keys() == {"clever"}
 	g.node[2]["funny"] = False
 	assert g.node[2].keys() == {"funny", "clever"}
-	orm.turn = 3
+	engy.turn = 3
 	assert not g.node[2]["funny"]
 	assert 3 not in g.node
-	orm.turn = 4
+	engy.turn = 4
 	assert g.node[2].keys() == {"funny", "clever"}
-	orm.turn = 1
-	orm.branch = "trunk"
-	orm.turn = 0
+	engy.turn = 1
+	engy.branch = "trunk"
+	engy.turn = 0
 	assert 1 not in g.node
-	orm.branch = "c"
-	orm.turn = 2
+	engy.branch = "c"
+	engy.turn = 2
 	assert 1 not in g.node
 	assert 2 not in g.node
-	orm.turn = 0
-	orm.branch = "trunk"
-	orm.turn = 2
+	engy.turn = 0
+	engy.branch = "trunk"
+	engy.turn = 2
 	assert 2 in g.node
 
 
