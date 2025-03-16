b111416731674fc9059b996850a95d062e22f9c3
Zachary Spector <public@zacharyspector.com>
2025-03-16 21:01:53 +1300

    Fix `test_multi_keyframe`

--- lisien/lisien/tests/test_load.py
+++ lisien/lisien/tests/test_load.py
@@ -283,7 +283,7 @@ def test_multi_keyframe(tmp_path):
 	]
 	eng.close()
 	eng = Engine(tmp_path, keyframe_on_close=False, workers=0)
-	eng._load_at("trunk", 0, tick0)
+	eng.load_at("trunk", 0, tick0)
 	assert eng._time_is_loaded("trunk", 0, tick0)
 	assert eng._time_is_loaded("trunk", 0, tick0 + 1)
 	assert eng._time_is_loaded("trunk", 1, tick1 - 1)
