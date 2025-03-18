f5704c3fe5b4e557faf8a6aab4442136c4cce2fc
Zachary Spector <public@zacharyspector.com>
2025-03-18 17:53:48 +1300

    Fix `test_future_contents` per new planning behavior

--- lisien/lisien/tests/test_thing.py
+++ lisien/lisien/tests/test_thing.py
@@ -53,11 +53,13 @@ def test_future_contents(something):
 	assert somebody not in somewhere.contents()
 	assert someone in somewhere.contents()
 	engine.turn = 1
+	engine.tick = engine.turn_end_plan()
 	assert len(somewhere.contents()) == 1
 	assert something not in somewhere.contents()
 	assert someone in somewhere.contents()
 	assert somebody not in somewhere.contents()
 	engine.turn = 2
+	engine.tick = engine.turn_end_plan()
 	assert len(somewhere.contents()) == 2
 	assert somebody in somewhere.contents()
 	assert someone in somewhere.contents()
