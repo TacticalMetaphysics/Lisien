d5c4740e0fe4ae2e3687c8708403f371536d5796
Zachary Spector <public@zacharyspector.com>
2025-04-05 05:53:42 +1300

    Change fixtures in `test_next_turn.py`
    Former-commit-id: 05e6586dc7d66382139faa59a08359e1db0f10e2

--- lisien/lisien/tests/test_next_turn.py
+++ lisien/lisien/tests/test_next_turn.py
@@ -20,115 +20,116 @@ it was followed got recorded correctly.
 """
 
 
-def test_character_dot_rule(sqleng):
+def test_character_dot_rule(proxyless_engine):
 	"""Test that a rule on a character is polled correctly"""
-	char = sqleng.new_character("who")
+	eng = proxyless_engine
+	char = eng.new_character("who")
 
 	@char.rule(always=True)
 	def yes(char):
 		char.stat["run"] = True
 
-	sqleng.next_turn()
-	btt = sqleng._btt()
+	eng.next_turn()
+	btt = eng._btt()
 	assert char.stat["run"]
-	sqleng.time = "trunk", 0, 0
+	eng.time = "trunk", 0, 0
 	assert "run" not in char.stat
-	sqleng.next_turn()
-	assert btt == sqleng._btt()
+	eng.next_turn()
+	assert btt == eng._btt()
 	assert char.stat["run"]
 
 
-def test_unit_dot_rule(sqleng):
+def test_unit_dot_rule(proxyless_engine):
 	"""Test that a rule applied to a character's avatars is polled correctly"""
-	char = sqleng.new_character("char")
-	graph = sqleng.new_character("graph")
+	char = proxyless_engine.new_character("char")
+	graph = proxyless_engine.new_character("graph")
 	av = graph.new_place("av")
 	char.add_unit(av)
-	starttick = sqleng.tick
+	starttick = proxyless_engine.tick
 
 	@char.unit.rule(always=True)
 	def yes(av):
 		av["run"] = True
 
-	sqleng.next_turn()
-	btt = sqleng._btt()
+	proxyless_engine.next_turn()
+	btt = proxyless_engine._btt()
 	assert av["run"]
-	sqleng.time = "trunk", 0, starttick
+	proxyless_engine.time = "trunk", 0, starttick
 	assert "run" not in av
-	sqleng.next_turn()
-	assert btt == sqleng._btt()
+	proxyless_engine.next_turn()
+	assert btt == proxyless_engine._btt()
 	assert av["run"]
 
 
-def test_thing_dot_rule(sqleng):
+def test_thing_dot_rule(proxyless_engine):
 	"""Test that a rule applied to a thing mapping is polled correctly"""
-	char = sqleng.new_character("char")
+	char = proxyless_engine.new_character("char")
 	place = char.new_place("place")
 	thing = place.new_thing("thing")
-	starttick = sqleng.tick
+	starttick = proxyless_engine.tick
 
 	@char.thing.rule(always=True)
 	def yes(thing):
 		thing["run"] = True
 
-	sqleng.next_turn()
-	btt = sqleng._btt()
+	proxyless_engine.next_turn()
+	btt = proxyless_engine._btt()
 	assert thing["run"]
-	sqleng.time = "trunk", 0, starttick
+	proxyless_engine.time = "trunk", 0, starttick
 	assert "run" not in thing
-	sqleng.next_turn()
-	assert btt == sqleng._btt()
+	proxyless_engine.next_turn()
+	assert btt == proxyless_engine._btt()
 	assert thing["run"]
 
 
-def test_place_dot_rule(sqleng):
+def test_place_dot_rule(proxyless_engine):
 	"""Test that a rule applied to a place mapping is polled correctly"""
-	char = sqleng.new_character("char")
+	char = proxyless_engine.new_character("char")
 	place = char.new_place("place")
-	starttick = sqleng.tick
+	starttick = proxyless_engine.tick
 
 	@char.place.rule(always=True)
 	def yes(plac):
 		plac["run"] = True
 
-	sqleng.next_turn()
-	btt = sqleng._btt()
+	proxyless_engine.next_turn()
+	btt = proxyless_engine._btt()
 	assert place["run"]
-	sqleng.time = "trunk", 0, starttick
+	proxyless_engine.time = "trunk", 0, starttick
 	assert "run" not in place
-	sqleng.next_turn()
-	assert btt == sqleng._btt()
+	proxyless_engine.next_turn()
+	assert btt == proxyless_engine._btt()
 	assert place["run"]
 
 
-def test_portal_dot_rule(sqleng):
+def test_portal_dot_rule(proxyless_engine):
 	"""Test that a rule applied to a portal mapping is polled correctly"""
-	char = sqleng.new_character("char")
+	char = proxyless_engine.new_character("char")
 	orig = char.new_place("orig")
 	dest = char.new_place("dest")
 	port = orig.new_portal(dest)
-	starttick = sqleng.tick
+	starttick = proxyless_engine.tick
 
 	@char.portal.rule(always=True)
 	def yes(portl):
 		portl["run"] = True
 
-	sqleng.next_turn()
-	btt = sqleng._btt()
+	proxyless_engine.next_turn()
+	btt = proxyless_engine._btt()
 	assert port["run"]
-	sqleng.time = "trunk", 0, starttick
+	proxyless_engine.time = "trunk", 0, starttick
 	assert "run" not in port
-	sqleng.next_turn()
-	assert btt == sqleng._btt()
+	proxyless_engine.next_turn()
+	assert btt == proxyless_engine._btt()
 	assert port["run"]
 
 
-def test_node_rule(sqleng):
+def test_node_rule(proxyless_engine):
 	"""Test that a rule applied to one node is polled correctly"""
-	char = sqleng.new_character("char")
+	char = proxyless_engine.new_character("char")
 	place = char.new_place("place")
 	thing = place.new_thing("thing")
-	starttick = sqleng.tick
+	starttick = proxyless_engine.tick
 
 	@place.rule(always=True)
 	def yes(plac):
@@ -138,47 +139,47 @@ def test_node_rule(sqleng):
 	def definitely(thig):
 		thig["run"] = True
 
-	sqleng.next_turn()
-	btt = sqleng._btt()
+	proxyless_engine.next_turn()
+	btt = proxyless_engine._btt()
 	assert place["run"]
 	assert thing["run"]
-	sqleng.time = "trunk", 0, starttick
+	proxyless_engine.time = "trunk", 0, starttick
 	assert "run" not in place
 	assert "run" not in thing
-	sqleng.next_turn()
-	assert btt == sqleng._btt()
+	proxyless_engine.next_turn()
+	assert btt == proxyless_engine._btt()
 	assert place["run"]
 	assert thing["run"]
 
 
-def test_portal_rule(sqleng):
+def test_portal_rule(proxyless_engine):
 	"""Test that a rule applied to one portal is polled correctly"""
-	char = sqleng.new_character("char")
+	char = proxyless_engine.new_character("char")
 	orig = char.new_place("orig")
 	dest = char.new_place("dest")
 	port = orig.new_portal(dest)
-	starttick = sqleng.tick
+	starttick = proxyless_engine.tick
 
 	@port.rule(always=True)
 	def yes(portl):
 		portl["run"] = True
 
-	sqleng.next_turn()
-	btt = sqleng._btt()
+	proxyless_engine.next_turn()
+	btt = proxyless_engine._btt()
 	assert port["run"]
-	sqleng.time = "trunk", 0, starttick
+	proxyless_engine.time = "trunk", 0, starttick
 	assert "run" not in port
-	sqleng.next_turn()
-	assert btt == sqleng._btt()
+	proxyless_engine.next_turn()
+	assert btt == proxyless_engine._btt()
 	assert port["run"]
 
 
-def test_post_time_travel_increment(sqleng):
+def test_post_time_travel_increment(proxyless_engine):
 	"""Test that, when the rules are run after time travel resulting in
 	a tick greater than zero, we advance to the next turn before running rules
 
 	"""
-	char = sqleng.new_character("char")
+	char = proxyless_engine.new_character("char")
 	char.stat["something"] = 0
 	place = char.new_place("there")
 	place["otherthing"] = 0
@@ -191,15 +192,15 @@ def test_post_time_travel_increment(sqleng):
 	def decr(plac):
 		plac["otherthing"] -= 1
 
-	sqleng.next_turn()
-	sqleng.next_turn()
-	assert sqleng.tick == 2
-	sqleng.branch = "branch1"
-	assert sqleng.tick == 2
-	sqleng.next_turn()
-	assert sqleng.tick == 2
-	sqleng.turn = 2
-	sqleng.branch = "trunk"
-	assert sqleng.tick == 2
-	sqleng.next_turn()
-	assert sqleng.tick == 2
+	proxyless_engine.next_turn()
+	proxyless_engine.next_turn()
+	assert proxyless_engine.tick == 2
+	proxyless_engine.branch = "branch1"
+	assert proxyless_engine.tick == 2
+	proxyless_engine.next_turn()
+	assert proxyless_engine.tick == 2
+	proxyless_engine.turn = 2
+	proxyless_engine.branch = "trunk"
+	assert proxyless_engine.tick == 2
+	proxyless_engine.next_turn()
+	assert proxyless_engine.tick == 2
