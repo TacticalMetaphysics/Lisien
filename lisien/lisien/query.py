758c0a4eb28d6ff8135fab09c58dd40ad18da191
Zachary Spector <public@zacharyspector.com>
2025-04-12 03:45:20 +1200

    Fix doubly munging rules-handled rows for the database
    Former-commit-id: d82954e11c0473a1b6e74d86860b135e175b1764

--- lisien/lisien/query.py
+++ lisien/lisien/query.py
@@ -8677,19 +8677,7 @@ class SQLAlchemyQueryEngine(AbstractQueryEngine):
 			)
 			self._new_keyframes = []
 
-			def munge(fields, rec):
-				return dict(zip(fields, rec))
-
-			def munger(*fields):
-				return partial(munge, fields)
-
-			def munged(fields, it):
-				return list(map(munger(*fields), it))
-
 			put = self._inq.put
-
-			def put_munged(stmt, fields, it):
-				put(("silent", "many", stmt, munged(fields, it)))
 
 			if self._unitness:
 				put(
@@ -8735,112 +8723,75 @@ class SQLAlchemyQueryEngine(AbstractQueryEngine):
 				put(("silent", "many", "things_insert", self._location))
 				self._location = []
 			if self._char_rules_handled:
-				put_munged(
-					"character_rules_handled_insert",
+				put(
 					(
-						"character",
-						"rulebook",
-						"rule",
-						"branch",
-						"turn",
-						"tick",
-					),
-					self._char_rules_handled,
+						"silent",
+						"many",
+						"character_rules_handled_insert",
+						self._char_rules_handled,
+					)
 				)
 				self._char_rules_handled = []
 			if self._char_thing_rules_handled:
-				put_munged(
-					"character_thing_rules_handled_insert",
+				put(
 					(
-						"character",
-						"thing",
-						"rulebook",
-						"rule",
-						"branch",
-						"turn",
-						"tick",
-					),
-					self._char_thing_rules_handled,
+						"silent",
+						"many",
+						"character_thing_rules_handled_insert",
+						self._char_thing_rules_handled,
+					)
 				)
 				self._char_thing_rules_handled = []
 
 			if self._char_place_rules_handled:
-				put_munged(
-					"character_place_rules_handled_insert",
+				put(
 					(
-						"character",
-						"place",
-						"rulebook",
-						"rule",
-						"branch",
-						"turn",
-						"tick",
-					),
-					self._char_place_rules_handled,
+						"silent",
+						"many",
+						"character_place_rules_handled_insert",
+						self._char_place_rules_handled,
+					)
 				)
 				self._char_place_rules_handled = []
 
 			if self._char_portal_rules_handled:
-				put_munged(
-					"character_portal_rules_handled_insert",
+				put(
 					(
-						"character",
-						"orig",
-						"dest",
-						"rulebook",
-						"rule",
-						"branch",
-						"turn",
-						"tick",
-					),
-					self._char_portal_rules_handled,
+						"silent",
+						"many",
+						"character_portal_rules_handled_insert",
+						self._char_portal_rules_handled,
+					)
 				)
 				self._char_portal_rules_handled = []
 			if self._unit_rules_handled:
-				put_munged(
-					"unit_rules_handled_insert",
+				put(
 					(
-						"character",
-						"graph",
-						"node",
-						"rulebook",
-						"rule",
-						"branch",
-						"turn",
-						"tick",
-					),
-					self._unit_rules_handled,
+						"silent",
+						"many",
+						"unit_rules_handled_insert",
+						self._unit_rules_handled,
+					)
 				)
 				self._unit_rules_handled = []
 			if self._node_rules_handled:
-				put_munged(
-					"node_rules_handled_insert",
+				put(
 					(
-						"character",
-						"node",
-						"rulebook",
-						"rule",
-						"branch",
-						"turn",
-						"tick",
-					),
-					self._node_rules_handled,
+						"silent",
+						"many",
+						"node_rules_handled_insert",
+						self._node_rules_handled,
+					)
 				)
 				self._node_rules_handled = []
 			if self._portal_rules_handled:
-				put_munged(
-					"portal_rules_handled_insert",
+				put(
 					(
-						"character",
-						"orig",
-						"dest",
-						"rulebook",
-						"rule",
-						"branch",
-						"turn",
-						"tick",
-					),
-					self._portal_rules_handled,
+						"silent",
+						"many",
+						"portal_rules_handled_insert",
+						self._portal_rules_handled,
+					)
 				)
 				self._portal_rules_handled = []
 			assert self.echo("flushed") == "flushed"
