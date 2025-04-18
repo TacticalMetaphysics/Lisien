cc2b7e47ca47441e6667d775973228a9ea87096a
Zachary Spector <public@zacharyspector.com>
2025-04-19 08:35:15 +1200

    Revert "Add a timer to demonstrate bad performance"
    This reverts commit 9998509f37179f5e5f15125fcb47cdf8ccb3b5ce.

--- lisien/lisien/facade.py
+++ lisien/lisien/facade.py
@@ -19,7 +19,6 @@ from .util import (
 	AbstractThing,
 	SignalDict,
 	getatt,
-	timer,
 )
 from .wrap import MutableMappingUnwrapper
 from .xcollections import CompositeDict
@@ -993,8 +992,6 @@ class CharacterFacade(AbstractCharacter):
 			else:
 				v.apply()
 		self.place._patch = {}
-		# Dictionary size changes during iteration below, suggesting that `_patch`
-		# isn't instance specific like it should be
 		for orig, dests in self.portal._patch.items():
 			for dest, v in dests.items():
 				if v is None:
@@ -1274,13 +1271,7 @@ class EngineFacade(AbstractEngine):
 			return
 		# Do I actually need these sorts? Insertion order's preserved...
 		for plan_num in sorted(self._planned):
-			with (
-				realeng.plan(),
-				timer(
-					f"facade committed plan {plan_num} to core",
-					logfun=self.log,
-				),
-			):
+			with realeng.plan():  # resets time at end of block
 				for turn in sorted(self._planned[plan_num]):
 					realeng.turn = turn
 					for tup in self._planned[plan_num][turn]:
