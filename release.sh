9a97b74f225f0b6739b1ee5bc83d584fbd14c30b
Zachary Spector <public@zacharyspector.com>
2025-04-26 08:19:43 +1200

    Use global twine
    Former-commit-id: eb9e71dbef9facba569a6ae3ad0cd791d9e0ee25

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ dos2unix -V
 alias python=python3.12
 python3.12 eqversion.py
 python -m build --version
-python -m twine --version
+twine --version
 python -m sphinx --version
 isort --version
 tox --version
@@ -25,9 +25,9 @@ rm -rf elide/build elide/dist elide/strings
 PYTHONPATH=$PWD/lisien:$PWD/elide python -m sphinx . docs/
 python -m build lisien/
 python -m build elide/
-python -m twine check lisien/dist/* elide/dist/*
-python -m twine upload lisien/dist/* elide/dist/*
-python -m twine upload --repository codeberg lisien/dist/* elide/dist/*
+twine check lisien/dist/* elide/dist/*
+twine upload lisien/dist/* elide/dist/*
+twine upload --repository codeberg lisien/dist/* elide/dist/*
 wine ~/lisien_windows/python/python.exe -m pip install --upgrade lisien/ elide/
 pyclean ~/lisien_windows
 unix2dos -n CHANGES.txt ~/lisien_windows/CHANGES.txt
