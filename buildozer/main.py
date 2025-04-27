1f05989152d6e7d7c29f910739f449355c16adef
Zachary Spector <public@zacharyspector.com>
2025-04-28 04:33:48 +1200

    Stop using `freeze_support` on Android

--- buildozer/main.py
+++ buildozer/main.py
@@ -1,1 +1,17 @@
-../main.py
+import os
+import sys
+
+wd = os.getcwd()
+sys.path.extend([wd + "/lisien", wd + "/elide"])
+
+
+def get_application_config(*args):
+	return wd + "/elide.ini"
+
+
+if __name__ == "__main__":
+	from elide.app import ElideApp
+
+	app = ElideApp()
+	app.get_application_config = get_application_config
+	app.run()
