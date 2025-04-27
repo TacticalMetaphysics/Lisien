5efe0b94e337de03aeb194d71259f18ff2c2b96f
Zachary Spector <public@zacharyspector.com>
2025-04-28 04:37:32 +1200

    Open Elide right away on Android
    Not necessarily the way I want to do things in the long run, but good for testing
    
    
    Former-commit-id: 199c4c4ae977e17ae40a272394492cb7f4a1be11

--- buildozer/main.py
+++ buildozer/main.py
@@ -1,8 +1,6 @@
 import os
-import sys
 
 wd = os.getcwd()
-sys.path.extend([wd + "/lisien", wd + "/elide"])
 
 
 def get_application_config(*args):
@@ -12,6 +10,6 @@ def get_application_config(*args):
 if __name__ == "__main__":
 	from elide.app import ElideApp
 
-	app = ElideApp()
+	app = ElideApp(path=wd)
 	app.get_application_config = get_application_config
 	app.run()
