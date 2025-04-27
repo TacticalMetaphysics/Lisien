a310bd156baf09810102e50ba91ed00f851d94a6
Zachary Spector <public@zacharyspector.com>
2025-04-27 16:18:25 +1200

    Check buildozer.spec version in eqversion.py
    And rename some vars
    
    
    Former-commit-id: 53864a80e40d17edc45970c313c71ce4fc8fbbcb

--- eqversion.py
+++ eqversion.py
@@ -2,11 +2,13 @@ import sys
 import tomllib
 
 with open("lisien/pyproject.toml", "rb") as inf:
-	lise_version = tomllib.load(inf)["project"]["version"]
+	lisien_version = tomllib.load(inf)["project"]["version"]
 with open("elide/pyproject.toml", "rb") as inf:
 	elide_version = tomllib.load(inf)["project"]["version"]
+with open("buildozer.spec", "rb") as inf:
+	buildozer_version = tomllib.load(inf)["app"]["version"]
 
-if lise_version != elide_version:
+if lisien_version != elide_version or lisien_version != buildozer_version:
 	sys.exit(
-		f"Version numbers differ. lisien: {lise_version}, elide: {elide_version}"
+		f"Version numbers differ. lisien: {lisien_version}, elide: {elide_version}, buildozer: {buildozer_version}"
 	)
