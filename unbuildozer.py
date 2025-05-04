6fa01fb23c6d8132bdac28c1ae3acb0afb457804
Zachary Spector <public@zacharyspector.com>
2025-05-05 06:19:07 +1200

    Don't worry if the app bundle doesn't exist in `unbuildozer.py`
    Former-commit-id: 8f60bfb195872978ca69dd4af060e509bd0cb960

--- unbuildozer.py
+++ unbuildozer.py
@@ -5,7 +5,7 @@ shutil.rmtree(
 	".buildozer/android/platform/build-arm64-v8a_armeabi-v7a/dists/Elide",
 	ignore_errors=True,
 )
-shutil.rmtree(".buildozer/android/app/elide")
+shutil.rmtree(".buildozer/android/app/elide", ignore_errors=True)
 pardir = ".buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/python-installs/Elide/armeabi-v7a/"
 for package in os.listdir(pardir):
 	if package.startswith("lisien") or package.startswith("elide"):
