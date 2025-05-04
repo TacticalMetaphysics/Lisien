9eb542c9be3ace1e99b7b2f72e63c2aad9ff5d0b
Zachary Spector <public@zacharyspector.com>
2025-05-05 04:38:55 +1200

    Make a script to clear out Lisien and Elide dists from .buildozer
    Former-commit-id: 799e768ef5a90e74dee30afbbf4104807f0cfa83

---
+++ unbuildozer.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+shutil.rmtree(
+	".buildozer/android/platform/build-arm64-v8a_armeabi-v7a/dists/Elide",
+	ignore_errors=True,
+)
+pardir = ".buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/python-installs/Elide/armeabi-v7a/"
+for package in os.listdir(pardir):
+	if package.startswith("lisien") or package.startswith("elide"):
+		abspath = os.path.join(pardir, package)
+		if (
+			not os.path.exists(abspath)
+			or not os.path.isdir(abspath)
+			or os.path.islink(abspath)
+		):
+			continue
+		shutil.rmtree(abspath)
