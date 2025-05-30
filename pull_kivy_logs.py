a214628368022d662ba21ed963f8e5aed480ac36
Zachary Spector <public@zacharyspector.com>
2025-05-31 10:18:01 +1200

    Make helper script to pull all kivy's logs out of my phone
    Former-commit-id: ac826db11505d3542b7b345d3754dc89f46b9c00

---
+++ pull_kivy_logs.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+
+
+filenames = list(
+	filter(
+		None,
+		subprocess.run(
+			[
+				"adb",
+				"shell",
+				"run-as org.tacmeta.elide ls files/app/.kivy/logs",
+			],
+			stdout=subprocess.PIPE,
+		)
+		.stdout.decode()
+		.split("\n"),
+	)
+)
+os.makedirs("kivylogs", exist_ok=True)
+for fn in filenames:
+	with open(os.path.join("kivylogs", fn), "wb") as outf:
+		outf.write(
+			subprocess.run(
+				[
+					"adb",
+					"shell",
+					"run-as org.tacmeta.elide cat files/app/.kivy/logs/" + fn,
+				],
+				stdout=subprocess.PIPE,
+			).stdout
+		)
