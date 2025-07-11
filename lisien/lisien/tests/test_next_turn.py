461fcc8c1689d7dbfa3c41c1974340fecce7146f
Zachary Spector <public@zacharyspector.com>
2025-07-11 13:35:14 +1200

    Fix stale fixture name in `test_next_turn`

--- lisien/lisien/tests/test_next_turn.py
+++ lisien/lisien/tests/test_next_turn.py
@@ -20,9 +20,9 @@ it was followed got recorded correctly.
 """
 
 
-def test_character_dot_rule(proxyless_engine):
+def test_character_dot_rule(engine):
 	"""Test that a rule on a character is polled correctly"""
-	eng = proxyless_engine
+	eng = engine
 	char = eng.new_character("who")
 
 	@char.rule(always=True)
@@ -39,97 +39,97 @@ def test_character_dot_rule(proxyless_engine):
 	assert char.stat["run"]
 
 
-def test_unit_dot_rule(proxyless_engine):
+def test_unit_dot_rule(engine):
 	"""Test that a rule applied to a character's avatars is polled correctly"""
-	char = proxyless_engine.new_character("char")
-	graph = proxyless_engine.new_character("graph")
+	char = engine.new_character("char")
+	graph = engine.new_character("graph")
 	av = graph.new_place("av")
 	char.add_unit(av)
-	starttick = proxyless_engine.tick
+	starttick = engine.tick
 
 	@char.unit.rule(always=True)
 	def yes(av):
 		av["run"] = True
 
-	proxyless_engine.next_turn()
-	btt = proxyless_engine._btt()
+	engine.next_turn()
+	btt = engine._btt()
 	assert av["run"]
-	proxyless_engine.time = "trunk", 0, starttick
+	engine.time = "trunk", 0, starttick
 	assert "run" not in av
-	proxyless_engine.next_turn()
-	assert btt == proxyless_engine._btt()
+	engine.next_turn()
+	assert btt == engine._btt()
 	assert av["run"]
 
 
-def test_thing_dot_rule(proxyless_engine):
+def test_thing_dot_rule(engine):
 	"""Test that a rule applied to a thing mapping is polled correctly"""
-	char = proxyless_engine.new_character("char")
+	char = engine.new_character("char")
 	place = char.new_place("place")
 	thing = place.new_thing("thing")
-	starttick = proxyless_engine.tick
+	starttick = engine.tick
 
 	@char.thing.rule(always=True)
 	def yes(thing):
 		thing["run"] = True
 
-	proxyless_engine.next_turn()
-	btt = proxyless_engine._btt()
+	engine.next_turn()
+	btt = engine._btt()
 	assert thing["run"]
-	proxyless_engine.time = "trunk", 0, starttick
+	engine.time = "trunk", 0, starttick
 	assert "run" not in thing
-	proxyless_engine.next_turn()
-	assert btt == proxyless_engine._btt()
+	engine.next_turn()
+	assert btt == engine._btt()
 	assert thing["run"]
 
 
-def test_place_dot_rule(proxyless_engine):
+def test_place_dot_rule(engine):
 	"""Test that a rule applied to a place mapping is polled correctly"""
-	char = proxyless_engine.new_character("char")
+	char = engine.new_character("char")
 	place = char.new_place("place")
-	starttick = proxyless_engine.tick
+	starttick = engine.tick
 
 	@char.place.rule(always=True)
 	def yes(plac):
 		plac["run"] = True
 
-	proxyless_engine.next_turn()
-	btt = proxyless_engine._btt()
+	engine.next_turn()
+	btt = engine._btt()
 	assert place["run"]
-	proxyless_engine.time = "trunk", 0, starttick
+	engine.time = "trunk", 0, starttick
 	assert "run" not in place
-	proxyless_engine.next_turn()
-	assert btt == proxyless_engine._btt()
+	engine.next_turn()
+	assert btt == engine._btt()
 	assert place["run"]
 
 
-def test_portal_dot_rule(proxyless_engine):
+def test_portal_dot_rule(engine):
 	"""Test that a rule applied to a portal mapping is polled correctly"""
-	char = proxyless_engine.new_character("char")
+	char = engine.new_character("char")
 	orig = char.new_place("orig")
 	dest = char.new_place("dest")
 	port = orig.new_portal(dest)
-	starttick = proxyless_engine.tick
+	starttick = engine.tick
 
 	@char.portal.rule(always=True)
 	def yes(portl):
 		portl["run"] = True
 
-	proxyless_engine.next_turn()
-	btt = proxyless_engine._btt()
+	engine.next_turn()
+	btt = engine._btt()
 	assert port["run"]
-	proxyless_engine.time = "trunk", 0, starttick
+	engine.time = "trunk", 0, starttick
 	assert "run" not in port
-	proxyless_engine.next_turn()
-	assert btt == proxyless_engine._btt()
+	engine.next_turn()
+	assert btt == engine._btt()
 	assert port["run"]
 
 
-def test_node_rule(proxyless_engine):
+def test_node_rule(engine):
 	"""Test that a rule applied to one node is polled correctly"""
-	char = proxyless_engine.new_character("char")
+	char = engine.new_character("char")
 	place = char.new_place("place")
 	thing = place.new_thing("thing")
-	starttick = proxyless_engine.tick
+	starttick = engine.tick
 
 	@place.rule(always=True)
 	def yes(plac):
@@ -139,47 +139,47 @@ def test_node_rule(proxyless_engine):
 	def definitely(thig):
 		thig["run"] = True
 
-	proxyless_engine.next_turn()
-	btt = proxyless_engine._btt()
+	engine.next_turn()
+	btt = engine._btt()
 	assert place["run"]
 	assert thing["run"]
-	proxyless_engine.time = "trunk", 0, starttick
+	engine.time = "trunk", 0, starttick
 	assert "run" not in place
 	assert "run" not in thing
-	proxyless_engine.next_turn()
-	assert btt == proxyless_engine._btt()
+	engine.next_turn()
+	assert btt == engine._btt()
 	assert place["run"]
 	assert thing["run"]
 
 
-def test_portal_rule(proxyless_engine):
+def test_portal_rule(engine):
 	"""Test that a rule applied to one portal is polled correctly"""
-	char = proxyless_engine.new_character("char")
+	char = engine.new_character("char")
 	orig = char.new_place("orig")
 	dest = char.new_place("dest")
 	port = orig.new_portal(dest)
-	starttick = proxyless_engine.tick
+	starttick = engine.tick
 
 	@port.rule(always=True)
 	def yes(portl):
 		portl["run"] = True
 
-	proxyless_engine.next_turn()
-	btt = proxyless_engine._btt()
+	engine.next_turn()
+	btt = engine._btt()
 	assert port["run"]
-	proxyless_engine.time = "trunk", 0, starttick
+	engine.time = "trunk", 0, starttick
 	assert "run" not in port
-	proxyless_engine.next_turn()
-	assert btt == proxyless_engine._btt()
+	engine.next_turn()
+	assert btt == engine._btt()
 	assert port["run"]
 
 
-def test_post_time_travel_increment(proxyless_engine):
+def test_post_time_travel_increment(engine):
 	"""Test that, when the rules are run after time travel resulting in
 	a tick greater than zero, we advance to the next turn before running rules
 
 	"""
-	char = proxyless_engine.new_character("char")
+	char = engine.new_character("char")
 	char.stat["something"] = 0
 	place = char.new_place("there")
 	place["otherthing"] = 0
@@ -192,15 +192,15 @@ def test_post_time_travel_increment(proxyless_engine):
 	def decr(plac):
 		plac["otherthing"] -= 1
 
-	proxyless_engine.next_turn()
-	proxyless_engine.next_turn()
-	assert proxyless_engine.tick == 2
-	proxyless_engine.branch = "branch1"
-	assert proxyless_engine.tick == 2
-	proxyless_engine.next_turn()
-	assert proxyless_engine.tick == 2
-	proxyless_engine.turn = 2
-	proxyless_engine.branch = "trunk"
-	assert proxyless_engine.tick == 2
-	proxyless_engine.next_turn()
-	assert proxyless_engine.tick == 2
+	engine.next_turn()
+	engine.next_turn()
+	assert engine.tick == 2
+	engine.branch = "branch1"
+	assert engine.tick == 2
+	engine.next_turn()
+	assert engine.tick == 2
+	engine.turn = 2
+	engine.branch = "trunk"
+	assert engine.tick == 2
+	engine.next_turn()
+	assert engine.tick == 2
