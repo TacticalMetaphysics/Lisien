f1a8e1eda329b0517f9237f8402dc8ba310e50a0
Zachary Spector <public@zacharyspector.com>
2025-07-13 12:39:39 +1200

    Check that we're not trying to republish the current version in `eqversion`

--- eqversion.py
+++ eqversion.py
@@ -1,23 +1,40 @@
+from io import StringIO
 import re
+import subprocess
 import sys
 import tomllib
 
 with open("lisien/pyproject.toml", "rb") as inf:
-	lisien_version = tomllib.load(inf)["project"]["version"]
+	lisien_version_str = tomllib.load(inf)["project"]["version"]
 with open("elide/pyproject.toml", "rb") as inf:
-	elide_version = tomllib.load(inf)["project"]["version"]
+	elide_version_str = tomllib.load(inf)["project"]["version"]
 with open("buildozer.spec", "rt") as inf:
 	for line in inf:
 		if line.startswith("version"):
-			buildozer_version = line.split("=")[-1].strip()
+			buildozer_version_str = line.split("=")[-1].strip()
 
 pat = r"(\d+?\.\d+?\.\d+?)(\.post[0-9]+)?"
+lisien_version = re.match(pat, lisien_version_str).group(1)
+elide_version = re.match(pat, elide_version_str).group(1)
+buildozer_version = re.match(pat, buildozer_version_str).group(1)
 
-if (
-	re.match(pat, lisien_version).group(1)
-	!= re.match(pat, elide_version).group(1)
-	or re.match(pat, lisien_version).group(1) != buildozer_version
-):
+if not (lisien_version == elide_version == buildozer_version):
 	sys.exit(
 		f"Version numbers differ. lisien: {lisien_version}, elide: {elide_version}, buildozer: {buildozer_version}"
 	)
+
+outtext = StringIO()
+proc = subprocess.call(
+	["python", "-m", "pip", "index", "versions", "lisien"], stdout=outtext
+)
+vers = ()
+for line in outtext.getvalue().split("\n"):
+	if line.startswith("Available versions: "):
+		vers = {
+			ver.strip()
+			for ver in line.removeprefix("Available versions: ").split(",")
+		}
+		break
+
+if lisien_version_str in vers:
+	sys.exit(f"Version {lisien_version_str} is already in PyPI.")
