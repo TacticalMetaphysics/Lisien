70f12e2fc3ef4e9e53a5a4adc235ae56a4e80e33
Zachary Spector <public@zacharyspector.com>
2025-07-29 20:47:20 +1200

    Clobber the journal when deleting anything
    Not as thorough a guard against bad journaling, but will work when loading simultaneous deletions

--- lisien/lisien/cache.py
+++ lisien/lisien/cache.py
@@ -1396,7 +1396,11 @@ class Cache:
 			# assert turn in presettings_turns \
 			# or turn in presettings_turns.future()
 			setticks = settings_turns[turn]
-			if not hasattr(self, "overwrite_journal") and tick in setticks:
+			if (
+				value is not None
+				and not hasattr(self, "overwrite_journal")
+				and tick in setticks
+			):
 				raise KeyError(
 					"Already have journal entry", self, branch, turn, tick
 				)
