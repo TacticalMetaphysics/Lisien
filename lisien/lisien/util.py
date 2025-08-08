9ee7552c093c86a1cb8417305f755d7bcc038d2f
Zachary Spector <public@zacharyspector.com>
2025-08-08 16:33:29 +1200

    Put EMPTY constant in util.py
    Former-commit-id: 234f4fa33a9d8365b754b619e254356bf248db8a

--- lisien/lisien/util.py
+++ lisien/lisien/util.py
@@ -96,6 +96,7 @@ if TYPE_CHECKING:
 TRUE: bytes = msgpack.packb(True)
 FALSE: bytes = msgpack.packb(False)
 NONE: bytes = msgpack.packb(None)
+EMPTY: bytes = msgpack.packb({})
 ELLIPSIS: bytes = b"\xc7\x00{"
 NAME: bytes = msgpack.packb("name")
 NODES: bytes = msgpack.packb("nodes")
