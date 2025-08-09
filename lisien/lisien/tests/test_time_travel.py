8723477aa34f02c99cded02b7a0da75d91276f67
Zachary Spector <public@zacharyspector.com>
2025-08-09 20:19:39 +1200

    Don't run `test_bookmark` on a coreless proxy

--- lisien/lisien/tests/test_time_travel.py
+++ lisien/lisien/tests/test_time_travel.py
@@ -63,14 +63,14 @@ def test_build_keyframe_window(null_engine):
 	)
 
 
-def test_bookmark(engy):
-	engy.bookmark("a")
-	engy.next_turn()
-	engy.bookmark("b")
-	engy.branch = "branch"
-	engy.bookmark("c")
-	assert engy.time == ("branch", 1, 0)
-	engy.bookmark("b")
-	assert engy.time == ("trunk", 1, 0)
-	engy.bookmark("a")
-	assert engy.time == ("trunk", 0, 0)
+def test_bookmark(engine):
+	engine.bookmark("a")
+	engine.next_turn()
+	engine.bookmark("b")
+	engine.branch = "branch"
+	engine.bookmark("c")
+	assert engine.time == ("branch", 1, 0)
+	engine.bookmark("b")
+	assert engine.time == ("trunk", 1, 0)
+	engine.bookmark("a")
+	assert engine.time == ("trunk", 0, 0)
