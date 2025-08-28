9574d5ca0b6429acd95b032fff7432c0c84a7b32
Zachary Spector <public@zacharyspector.com>
2025-08-28 23:04:02 +1200

    Port `test_python_editor` to pytest

--- elide/elide/tests/test_python_editor.py
+++ elide/elide/tests/test_python_editor.py
@@ -1,70 +1,70 @@
+import pytest
+
 from lisien import Engine
 from lisien.examples import sickle
 
-from .util import ELiDEAppTest, idle_until
+from .util import idle_until
 
 
-class PythonEditorTest(ELiDEAppTest):
-	install = staticmethod(sickle.install)
-
-	def _get_actions_box(self):
-		app = self.app
-		idle_until(
-			lambda: hasattr(app, "mainscreen")
-			and app.mainscreen.mainview
-			and app.mainscreen.statpanel
-			and hasattr(app.mainscreen, "gridview")
-		)
-		app.funcs.toggle()
-		idle_until(
-			lambda: "actions" in app.funcs.ids, 100, "Never got actions box"
-		)
-		actions_box = app.funcs.ids.actions
-		idle_until(lambda: actions_box.editor, 100, "Never got FuncEditor")
-		idle_until(lambda: actions_box.storelist, 100, "Never got StoreList")
-		idle_until(
-			lambda: actions_box.storelist.data, 100, "Never got StoreList data"
-		)
-		return actions_box
+def get_actions_box(elide_app):
+	app = elide_app
+	idle_until(
+		lambda: hasattr(app, "mainscreen")
+		and app.mainscreen.mainview
+		and app.mainscreen.statpanel
+		and hasattr(app.mainscreen, "gridview")
+	)
+	app.funcs.toggle()
+	idle_until(
+		lambda: "actions" in app.funcs.ids, 100, "Never got actions box"
+	)
+	actions_box = app.funcs.ids.actions
+	idle_until(lambda: actions_box.editor, 100, "Never got FuncEditor")
+	idle_until(lambda: actions_box.storelist, 100, "Never got StoreList")
+	return actions_box
 
 
-class TestShowCode(PythonEditorTest):
-	def test_show_code(self):
-		app = self.app
-		self.Window.add_widget(app.build())
-		actions_box = self._get_actions_box()
-		last = actions_box.storelist.data[-1]["name"]
-		actions_box.storelist.selection_name = last
-		idle_until(
-			lambda: "funname" in actions_box.editor.ids,
-			100,
-			"Never got function input widget",
-		)
-		idle_until(
-			lambda: actions_box.editor.ids.funname.hint_text,
-			100,
-			"Never got function name",
-		)
-		idle_until(
-			lambda: "code" in actions_box.editor.ids,
-			100,
-			"Never got code editor widget",
-		)
-		idle_until(
-			lambda: actions_box.editor.ids.code.text,
-			100,
-			"Never got source code",
-		)
+@pytest.fixture
+def sickle_sim(play_dir):
+	with Engine(play_dir, workers=0) as eng:
+		sickle.install(eng)
 
 
-class TestCreateAction(PythonEditorTest):
-	def test_create_action(self):
-		app = self.app
-		self.Window.add_widget(app.build())
-		actions_box = self._get_actions_box()
-		actions_box.editor.ids.funname.text = "new_func"
-		actions_box.editor.ids.code.text = 'return "Hello, world!"'
-		app.stop()
-		assert app.stopped
-		with Engine(self.engine_prefix) as eng:
-			assert hasattr(eng.action, "new_func")
+@pytest.mark.usefixtures("sickle_sim")
+def test_show_code(elide_app):
+	app = elide_app
+	actions_box = get_actions_box(app)
+	last = actions_box.storelist.data[-1]["name"]
+	actions_box.storelist.selection_name = last
+	idle_until(
+		lambda: "funname" in actions_box.editor.ids,
+		100,
+		"Never got function input widget",
+	)
+	idle_until(
+		lambda: actions_box.editor.ids.funname.hint_text,
+		100,
+		"Never got function name",
+	)
+	idle_until(
+		lambda: "code" in actions_box.editor.ids,
+		100,
+		"Never got code editor widget",
+	)
+	idle_until(
+		lambda: actions_box.editor.ids.code.text,
+		100,
+		"Never got source code",
+	)
+
+
+@pytest.mark.usefixtures("kivy")
+def test_create_action(play_dir, elide_app):
+	app = elide_app
+	actions_box = get_actions_box(app)
+	actions_box.editor.ids.funname.text = "new_func"
+	actions_box.editor.ids.code.text = 'return "Hello, world!"'
+	app.stop()
+	assert app.stopped
+	with Engine(play_dir, workers=0) as eng:
+		assert hasattr(eng.action, "new_func")
