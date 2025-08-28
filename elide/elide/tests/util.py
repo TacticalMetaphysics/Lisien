b149df5d74bda094ad7b182beae5c1a553e0a8bd
Zachary Spector <public@zacharyspector.com>
2025-08-28 21:37:42 +1200

    Reapply "Make my own `elide_app` test fixture and port `test_character_switcher` to it"
    This reverts commit 9802ff29d1a8b53578780bb07fc9c3974aad35fe [formerly 452aa9dffb8c966141ea1e64cf049921b4b81420].
    
    
    Former-commit-id: d7ad95910b36428983503532cc14a6fd8daaa8d4

--- elide/elide/tests/util.py
+++ elide/elide/tests/util.py
@@ -131,6 +131,13 @@ class MockEngine(Signal):
 		pass
 
 
+def advance_frames(frames: int) -> None:
+	from kivy.base import EventLoop
+
+	for _ in range(frames):
+		EventLoop.idle()
+
+
 class ELiDEAppTest(GraphicUnitTest):
 	games_dir = "games"
 	game_name = "test"
