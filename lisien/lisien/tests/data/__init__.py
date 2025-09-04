a50beb6aba93fd3e466de5a3835d41d3edf3584d
Zachary Spector <public@zacharyspector.com>
2025-09-05 10:11:37 +1200

    Test XML export
    Former-commit-id: 64b657c28199abc158b0c172a24cd2b600646da5

--- lisien/lisien/tests/data/__init__.py
+++ lisien/lisien/tests/data/__init__.py
@@ -12,7 +12,11 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import os
+
 from lisien.facade import EngineFacade
+
+DATA_DIR = os.path.dirname(__file__)
 
 CHARACTER_UPDATES = [
 	("empty", {}, {}, [], [], [], []),
--- lisien/lisien/tests/test_export.py
+++ lisien/lisien/tests/test_export.py
@@ -1,18 +1,30 @@
+import filecmp
+import os
+from functools import partial
+
 import pytest
 
 from lisien import Engine
-from lisien.examples import kobold, polygons, wolfsheep
-from lisien.export import game_path_to_etree
+from lisien.export import game_path_to_xml
+from lisien.tests.data import DATA_DIR
 
 
-@pytest.fixture(params=["kobold", "polygons", "wolfsheep"], autouse=True)
-def sim(tmp_path, random_seed, request):
+@pytest.fixture(params=["kobold", "polygons", "wolfsheep"])
+def exported(tmp_path, random_seed, request):
 	if request.param == "kobold":
-		install = kobold.inittest
+		from lisien.examples.kobold import inittest as install
+
+		path = os.path.join(DATA_DIR, "kobold.xml")
 	elif request.param == "polygons":
-		install = polygons.install
+		from lisien.examples.polygons import install
+
+		path = os.path.join(DATA_DIR, "polygons.xml")
 	elif request.param == "wolfsheep":
-		install = wolfsheep.install
+		from lisien.examples.wolfsheep import install
+
+		install = partial(install, seed=random_seed)
+
+		path = os.path.join(DATA_DIR, "wolfsheep.xml")
 	else:
 		raise ValueError("Unknown sim", request.param)
 	with Engine(
@@ -22,13 +34,13 @@ def sim(tmp_path, random_seed, request):
 		connect_string=f"sqlite:///{tmp_path}/world.sqlite3",
 	) as eng:
 		install(eng)
-		for _ in range(10):
+		for _ in range(1):
 			eng.next_turn()
+	yield path
 
 
-def test_export(tmp_path):
-	from io import BytesIO
+def test_export(tmp_path, exported):
+	test_xml = os.path.join(tmp_path, "test.xml")
+	game_path_to_xml(tmp_path, test_xml)
 
-	f = BytesIO()
-	game_path_to_etree(tmp_path).write(f)
-	print(f.getvalue().decode())
+	assert filecmp.cmp(test_xml, exported)
