827b7a4e7acbf609366af243eaaa73e9a2c11974
Zachary Spector <public@zacharyspector.com>
2025-09-16 19:35:57 +1200

    Fix `test_export_db`
    Former-commit-id: 093d2e1a342a601869a5a91c8d8a6f325a7e3548

--- lisien/lisien/tests/test_import_export.py
+++ lisien/lisien/tests/test_import_export.py
@@ -54,7 +54,9 @@ def exported(tmp_path, random_seed, non_null_database, request):
 
 def test_export_db(tmp_path, exported):
 	test_xml = os.path.join(tmp_path, "test.xml")
-	game_path_to_xml(tmp_path, test_xml, name="test_export")
+	game_path_to_xml(
+		os.path.join(tmp_path, "game"), test_xml, name="test_export"
+	)
 
 	assert filecmp.cmp(test_xml, exported)
 
