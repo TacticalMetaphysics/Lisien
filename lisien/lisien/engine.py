8612e93a75e61b24fa8e6ac6e98ef201956a21b0
Zachary Spector <public@zacharyspector.com>
2025-09-16 13:26:05 +1200

    Import to parquetdb when the connect string is `None`
    Rather than absent entirely
    
    
    Former-commit-id: 71a58b98b52c26cd690b311ebb1aab272b7a53d6

--- lisien/lisien/engine.py
+++ lisien/lisien/engine.py
@@ -7289,7 +7289,7 @@ class Engine(AbstractEngine, Executor):
 		extracted = os.listdir(prefix)
 		if "world.xml" in extracted:
 			xml_path = os.path.join(prefix, "world.xml")
-			if "connect_string" in kwargs:
+			if "connect_string" in kwargs and kwargs["connect_string"]:
 				xml_to_sqlite(xml_path, os.path.join(prefix, "world.sqlite3"))
 			else:
 				os.makedirs(os.path.join(prefix, "world"), exist_ok=True)
