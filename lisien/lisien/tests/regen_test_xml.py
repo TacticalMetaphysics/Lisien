7e38703e2b44610bc426ed0b4a8943308282cdf3
Zachary Spector <public@zacharyspector.com>
2025-10-10 12:11:36 +1300

    Write `regen_test_xml.py`
    I'd been making `test_export_db` regenerate it for me, but that practice led me to dummying it out and leaving the dummied version in a release by mistake.

---
+++ lisien/lisien/tests/regen_test_xml.py
@@ -0,0 +1,38 @@
+from functools import partial
+import os
+from tempfile import TemporaryDirectory
+from lisien.engine import Engine
+from lisien.exporter import game_path_to_xml
+from lisien.tests.data import DATA_DIR
+
+RANDOM_SEED = 69105
+TURNS = 1
+
+for sim in ["kobold", "polygons", "wolfsheep"]:
+	if sim == "kobold":
+		from lisien.examples.kobold import inittest as install
+	elif sim == "polygons":
+		from lisien.examples.polygons import install
+	elif sim == "wolfsheep":
+		from lisien.examples.wolfsheep import install
+
+		install = partial(install, seed=RANDOM_SEED)
+	else:
+		raise RuntimeError("Unknown sim", sim)
+	with TemporaryDirectory() as tmp_path:
+		prefix = os.path.join(tmp_path, "game")
+		with Engine(
+			prefix,
+			workers=0,
+			random_seed=RANDOM_SEED,
+			connect_string=f"sqlite:///{prefix}/world.sqlite3",
+			keyframe_on_close=False,
+		) as eng:
+			install(eng)
+			for _ in range(TURNS):
+				eng.next_turn()
+		game_path_to_xml(
+			prefix,
+			os.path.join(DATA_DIR, sim + ".xml"),
+			name="test_export",
+		)
