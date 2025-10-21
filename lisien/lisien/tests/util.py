bcd41db28f39a9b77882b7e3124fdc18ab1e60d0
Zachary Spector <public@zacharyspector.com>
2025-10-21 18:38:25 +1300

    Add `test_start` to `test_proxy`
    Add neat new test utility `fail_after`
    
    Skip testing parquetdb in subinterpreters for now; PyArrow doesn't support it
    
    
    Former-commit-id: eec47e3d3ac5fda9121c30e225d49055b0b50b56

--- lisien/lisien/tests/util.py
+++ lisien/lisien/tests/util.py
@@ -1,8 +1,59 @@
+from functools import wraps
+import inspect
+from queue import SimpleQueue, Empty
+from threading import Thread
+import time
+from typing import Any, Callable, Generic
 from unittest.mock import MagicMock
 
 from lisien import Engine
 from lisien.facade import EngineFacade
 from lisien.proxy.manager import Sub
+
+
+_RETURNS = Generic()
+
+
+def fail_after(
+	timeout: float, now: bool = True
+) -> Callable[[Callable[[], _RETURNS]], _RETURNS]:
+	"""If the wrapped function doesn't finish before the timeout, fail the test.
+
+	This executes the wrapped function in a daemonic subthread. If the function
+	doesn't exit, neither will the subthread, which may cause pytest headaches
+	if that thread keeps running after all the tests are done.
+
+	By default, this immediately starts running the wrapped function. If you
+	don't want this, set ``now=False``.
+
+	"""
+
+	def call_and_put_return_value(
+		func: Callable[[], Any], q: SimpleQueue
+	) -> None:
+		q.put(func())
+
+	def fail_after_decorator(
+		func: Callable[[], _RETURNS],
+	) -> Callable[[], _RETURNS]:
+		@wraps(func)
+		def fails():
+			q: SimpleQueue = SimpleQueue()
+			call_thread = Thread(
+				target=call_and_put_return_value, args=(func, q), daemon=True
+			)
+			call_thread.start()
+			try:
+				ret = q.get(timeout=timeout)
+			except Empty as ex:
+				raise TimeoutError(f"{func!r} timed out") from ex
+			return ret
+
+		if now:
+			fails()
+		return fails
+
+	return fail_after_decorator
 
 
 def make_test_engine_kwargs(
