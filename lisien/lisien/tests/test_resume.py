40eed38b06d4e8bec0985ca227ff086f64f2a839
Zachary Spector <public@zacharyspector.com>
2025-10-24 19:52:19 +1300

    Use PythonDatabaseConnector in tests, refactor some
    And, turns out tests mostly aren't passing.
    
    
    Former-commit-id: a9434ba65b055e73e4ecdb27f91e90d9bda8c6ec

--- lisien/lisien/tests/test_resume.py
+++ lisien/lisien/tests/test_resume.py
@@ -8,9 +8,9 @@ from .util import make_test_engine_kwargs
 
 # TODO: use a test sim that does everything in every cache
 @pytest.mark.big
-def test_resume(tmp_path, non_null_database):
+def test_resume(tmp_path, persistent_database):
 	ekwargs = make_test_engine_kwargs(
-		tmp_path, "serial", non_null_database, 69105
+		tmp_path, "serial", persistent_database, 69105
 	)
 	ekwargs["keyframe_on_close"] = False
 	with Engine(**ekwargs) as eng:
