33d2bd68b491320ed1e68ab55141e89574b67068
Zachary Spector <public@zacharyspector.com>
2025-10-28 12:44:49 +1300

    Fix starting worker interpreters
    Former-commit-id: 765ccdaf59d541e7e1f3e6ac405c82169dde4f08

---
+++ lisien/lisien/proxy/worker_subinterpreter.py
@@ -0,0 +1,31 @@
+def worker_subinterpreter(
+	i: int,
+	prefix: str,
+	branches: dict,
+	eternal: dict,
+	in_queue,
+	out_queue,
+	logq,
+	*,
+	function: dict | None,
+	method: dict | None,
+	trigger: dict | None,
+	prereq: dict | None,
+	action: dict | None,
+):
+	from lisien.proxy.worker_subroutine import worker_subroutine
+
+	return worker_subroutine(
+		i,
+		prefix,
+		branches,
+		eternal,
+		in_queue.get,
+		out_queue.put,
+		logq,
+		function=function,
+		method=method,
+		trigger=trigger,
+		prereq=prereq,
+		action=action,
+	)
