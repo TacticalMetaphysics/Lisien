f9499ac4310717925e5bd68c116a53fd993f44cc
Zachary Spector <public@zacharyspector.com>
2025-10-29 07:02:32 +1300

    Stop doing that weird time warp when closing an engine
    Former-commit-id: 5f3e01b5850765d4ab84bc718120536d65a6d434

--- lisien/lisien/engine.py
+++ lisien/lisien/engine.py
@@ -5812,9 +5812,6 @@ class Engine(AbstractEngine, Executor):
 		"""
 		if hasattr(self, "_closed"):
 			raise RuntimeError("Already closed")
-		time_was: tuple[Turn, Tick] = (self._oturn, self._otick)
-		if time_was > self._branch_end():
-			(self.turn, self.tick) = self._branch_end()
 		if (
 			self._keyframe_on_close
 			and self._btt() not in self._keyframes_times
@@ -5825,7 +5822,6 @@ class Engine(AbstractEngine, Executor):
 				)
 			else:
 				self.snap_keyframe(silent=True, update_worker_processes=False)
-		(self._oturn, self._otick) = time_was
 		for store in self.stores:
 			if hasattr(store, "save"):
 				store.save(reimport=False)
