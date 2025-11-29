41dfbe9ea43a24599759693e7c849398e3d8cd13
Zachary Spector <public@zacharyspector.com>
2025-11-29 17:37:43 +1300

    Remove typo
    Former-commit-id: e18e3b6b3fb72aa85a76b3b7860d9304618cc656

--- lisien/lisien/types.py
+++ lisien/lisien/types.py
@@ -11,7 +11,7 @@
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.f
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from __future__ import annotations
 
 import builtins
