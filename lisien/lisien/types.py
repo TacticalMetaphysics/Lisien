44bf530e6379b9edafca07d9c259659ba40033d9
Zachary Spector <public@zacharyspector.com>
2025-12-09 15:44:10 +1300

    Fix `UnpackSignature`
    Former-commit-id: c28b86d31db8ea546d647838080cd9187ec90ee8

--- lisien/lisien/types.py
+++ lisien/lisien/types.py
@@ -2237,7 +2237,7 @@ type PackSignature = Callable[
 	[Key | KeyHint | EternalKey | UniversalKey | Stat | ValueHint | Value],
 	bytes,
 ]
-type UnpackSignature = Callable[[bytes], ValueHint]
+type UnpackSignature = Callable[[bytes], Value]
 type LoadedCharWindow = dict[
 	Literal[
 		"nodes",
