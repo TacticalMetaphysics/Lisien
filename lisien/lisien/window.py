9fc7bc9def4a28169b387996b5a8fae524668f7a
Zachary Spector <public@zacharyspector.com>
2025-12-12 17:18:57 +1300

    Don't use attrs `__repr__` for `WindowDict`
    Former-commit-id: f3e8d0d9dfc236034fb6bd6bea6fb8f1af73394c

--- lisien/lisien/window.py
+++ lisien/lisien/window.py
@@ -612,7 +612,7 @@ def _recurse(rev: _RK, revs: list[tuple[_RK, _RV]]) -> tuple[_RK, _RV]:
 		return _recurse(rev, after)
 
 
-@define(init=False, eq=False)
+@define(init=False, eq=False, repr=False)
 class WindowDict[_K: int, _V: ValueHint | WindowDict](MutableMapping[_K, _V]):
 	"""A dict that keeps every value that a variable has had over time.
 
