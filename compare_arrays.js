342ac1366656a3ccbfcd4439ec77564559d4a9f2
Zachary Spector <public@zacharyspector.com>
2026-01-04 18:41:40 +1300

    Add `compare_lists` for micropython compat

--- compare_arrays.js
+++ compare_arrays.js
@@ -5,4 +5,8 @@ export function compare_arrays(array1, array2) {
     })
 }
 
+export function compare_lists(list1, list2) {
+    return compare_arrays(Array.from(list1), Array.from(list2))
+}
+
 
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from pyscript import document
-from pyscript.js_modules.compare_arrays import compare_arrays
+from pyscript.js_modules.compare_arrays import compare_lists
 
 
 array0 = [1, 2, 4, 8, 15, 16, 23, 42]
 array1 = [6, 5, 4, 3, 16, 16, 13, 42]
 
-document.body.append(repr(compare_arrays(array0, array1)))
+document.body.append(repr(list(compare_lists(array0, array1))))
