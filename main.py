abb36f59edf4f27879a9384ae41f547298bd363a
Zachary Spector <public@zacharyspector.com>
2026-01-04 19:23:03 +1300

    Fetch my packages thru the API
    I want to fetch Lisien itself this way

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-from pyscript import document
-from pyscript.js_modules.compare_arrays import compare_lists
+from pyscript import document, fetch
 
 
-array0 = [1, 2, 4, 8, 15, 16, 23, 42]
-array1 = [6, 5, 4, 3, 16, 16, 13, 42]
+CODEBERG_API_KEY = "a623f45a789dc58dc5f8bde889a9e1fefbba5f53"
 
-document.body.append(repr(list(compare_lists(array0, array1))))
+print("fetching...")
+fetching = await fetch(
+	"https://codeberg.org/api/v1/packages/clayote?type=pypi",
+	method="GET",
+	headers={
+		"Authorization": CODEBERG_API_KEY,
+		"Accept": "application/json",
+	},
+)
+
+try:
+	if fetching.ok:
+		interpreted = await fetching.text()
+		print("fetched", str(interpreted))
+	else:
+		print("not ok", fetching.status)
+except Exception as ex:
+	print(ex)
+	print(fetching)
+	print("not fetched")
