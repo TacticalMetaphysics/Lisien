97a3e16cfdb2e0687904a2d6a91a946f9629a077
Zachary Spector <public@zacharyspector.com>
2026-01-04 17:55:41 +1300

    Append the comparison to the document, so I don't have to open the console

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from pyscript import document
 from pyscript.js_modules import compare_arrays
 
 
 array0 = [1, 2, 4, 8, 15, 16, 23, 42]
 array1 = [6, 5, 4, 3, 16, 16, 13, 42]
 
-print(compare_arrays(array0, array1))
+document.body.append(repr(compare_arrays(array0, array1)))
