c85ee02c2fa11d1e3a4fecb797b6f079136d8d07
Zachary Spector <public@zacharyspector.com>
2026-01-06 17:27:03 +1300

    Test the sickle example in the browser
    Currently, it runs correctly, but doesn't export right.

--- main.py
+++ main.py
@@ -6,8 +6,18 @@ import micropip
 
 await micropip.install("annotated-types")
 
-from lisien import types
+from lisien import Engine
+from lisien.examples import sickle
 
+with Engine(workers=0) as eng:
+	sickle.install(eng)
+	for i in range(50):
+		print(i)
+		eng.next_turn()
+	eng.export("sickle")
 
-CODEBERG_API_KEY = "a623f45a789dc58dc5f8bde889a9e1fefbba5f53"
-print(dir(types))
+with Engine.from_archive("sickle.lisien", workers=0) as eng:
+	print(list(eng.character))
+	print("critters alive:")
+	for node in eng.character["physical"].thing:
+		print(node)
