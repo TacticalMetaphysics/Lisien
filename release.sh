01a962d0de6991c3a0619d921f82f56fb60397e3
Zachary Spector <public@zacharyspector.com>
2026-01-06 18:07:25 +1300

    Change docs path in release.sh
    Former-commit-id: 43dd3933c855418d6d19b1b8b95061c61a028eac

--- release.sh
+++ release.sh
@@ -23,7 +23,7 @@ isort lisien
 isort elide
 ruff format lisien
 ruff format elide
-PYTHONPATH=$PWD/lisien:$PWD/elide python -m sphinx . docs/
+PYTHONPATH=$PWD/lisien:$PWD/elide python -m sphinx . pages/docs/
 for env in $(tox -c elide/tox.ini -l); do if [ -e "/tmp/pytest-of-${USER}" ]; then rm -r "/tmp/pytest-of-${USER}/"; fi; pyclean .; tox -c elide/tox.ini -e $env; done
 for env in $(tox -c lisien/tox.ini -l); do if [ -e "/tmp/pytest-of-${USER}" ]; then rm -r "/tmp/pytest-of-${USER}/"; fi; pyclean .; tox -c lisien/tox.ini -e $env; done
 rm -rf bin lisien/dist elide/dist
