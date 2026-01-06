6587745d58abb850f3fb81c68922ecfc4f56af44
Zachary Spector <public@zacharyspector.com>
2026-01-06 13:09:18 +1300

    Switch to pyodide, make a zip bundle that imports cleanly
    micropython lacks the `concurrent` module, or even thread support?? At least, the version currently bundled in pyscript doesn't have the APIs I need

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
+from pyodide_js import loadPackage
 from pyscript import document, fetch
+
+await loadPackage("micropip")
+import micropip
+
+await micropip.install("annotated-types")
+
+from lisien import types
 
 
 CODEBERG_API_KEY = "a623f45a789dc58dc5f8bde889a9e1fefbba5f53"
-
-print("fetching...")
-fetching = await fetch(
-	"https://codeberg.org/api/v1/packages/clayote?type=pypi",
-	method="GET",
-	headers={
-		"Authorization": CODEBERG_API_KEY,
-		"Accept": "application/json",
-	},
-)
-
-try:
-	if fetching.ok:
-		interpreted = await fetching.text()
-		print("fetched", str(interpreted))
-	else:
-		print("not ok", fetching.status)
-except Exception as ex:
-	print(ex)
-	print(fetching)
-	print("not fetched")
+print(dir(types))
