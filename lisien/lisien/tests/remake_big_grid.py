32bba68a690064686880ae42d19f7edfe32f0e01
Zachary Spector <public@zacharyspector.com>
2026-01-08 09:48:50 +1300

    Add a performance test for parallelism
    I found out that parallelism has been broken for a while, but all my tests for it pass, because the way I implemented *serial* operation looks exactly the same to the tests.
    
    A test focusing on performance is not so easily fooled.
    
    I've made the performance target quite relaxed, since all I really care about is whether it was run in parallel or not.

--- lisien/lisien/tests/remake_big_grid.py
+++ lisien/lisien/tests/remake_big_grid.py
@@ -11,6 +11,13 @@ def main():
 	big_grid.add_node("them", location=(0, 0))
 	big_grid.graph["straightly"] = nx.shortest_path(big_grid, (0, 0), (99, 99))
 	with Engine(None, workers=0) as eng:
+
+		@eng.method
+		def find_path(eng, node_a, node_b):
+			from networkx import shortest_path
+
+			return shortest_path(eng.character["grid"], node_a, node_b)
+
 		eng.add_character("grid", big_grid)
 		eng.export("grid", os.path.join(DATA_DIR, "big_grid.lisien"))
 
