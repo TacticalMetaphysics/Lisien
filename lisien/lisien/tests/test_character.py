9acc143625662388b46bf0027f0bd4009b8337bc
Zachary Spector <public@zacharyspector.com>
2026-01-16 10:34:16 +1300

    Share a set of `ProxyManager`s between the tests
    Instead of making special test-only proxies to the worker pools, like before

--- lisien/lisien/tests/test_character.py
+++ lisien/lisien/tests/test_character.py
@@ -154,7 +154,7 @@ def update_char(char: AbstractCharacter, *, stat=(), nodes=(), portals=()):
 	CHARACTER_UPDATES,
 )
 def test_char_creation(
-	engy, name, data, stat, nodestat, statup, nodeup, edgeup
+	engine, name, data, stat, nodestat, statup, nodeup, edgeup
 ):
 	char = engy.new_character(name, data, **stat)
 	assert set(char.node) == set(data)
