c66c4e5e85c1e2db9b22477cce602499ddaa63d9
Zachary Spector <public@zacharyspector.com>
2026-01-19 17:00:14 +1300

    Remove annotationlib import

--- lisien/lisien/types.py
+++ lisien/lisien/types.py
@@ -14,7 +14,6 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from __future__ import annotations
 
-import annotationlib
 import builtins
 import inspect
 import operator
