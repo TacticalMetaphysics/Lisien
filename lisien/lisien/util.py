2296b7f573e4d0e264fe07334c906ed5662560e4
Zachary Spector <public@zacharyspector.com>
2026-01-26 13:02:42 +1300

    Clean up in `types.py`, add some docstrings to types
    Former-commit-id: a539932c0c7e2cdbad94e5d740d930fc013707ce

--- lisien/lisien/util.py
+++ lisien/lisien/util.py
@@ -12,8 +12,6 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""Common utility functions and data structures."""
-
 from __future__ import annotations
 
 import gc
