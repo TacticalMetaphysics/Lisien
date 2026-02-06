d28cf0a055b2b7a19089b6f7986e31d6074efea2
Zachary Spector <public@zacharyspector.com>
2026-02-06 13:25:45 +1300

    Switch to environment variables for `twine` in release.sh
    Former-commit-id: 4e740e82048a6a79b6982cc28ff53b60336b6f90

--- release.sh
+++ release.sh
@@ -47,8 +47,8 @@ git push --tags
 python -m build lisien/
 python -m build elide/
 twine check lisien/dist/* elide/dist/*
-twine upload lisien/dist/* elide/dist/*
-twine upload --repository codeberg lisien/dist/* elide/dist/*
+TWINE_USERNAME=$CODEBERG_USERNAME TWINE_PASSWORD=$CODEBERG_PASSWORD twine upload lisien/dist/* elide/dist/*
+TWINE_USERNAME=$PYPI_USERNAME TWINE_PASSWORD=$PYPI_PASSWORD twine upload lisien/dist/* elide/dist/*
 wine lisien_windows/python/python.exe -m pip install --force-reinstall lisien/ elide/ 'parquetdb @ git+https://github.com/lllangWV/ParquetDB.git'
 pyclean lisien_windows
 unix2dos -n CHANGES.txt lisien_windows/CHANGES.txt
