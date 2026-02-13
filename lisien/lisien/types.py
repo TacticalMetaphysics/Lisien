9f5a32f9a894e8f5a6bd37298fd08d779f0bc637
Zachary Spector <public@zacharyspector.com>
2026-02-13 17:12:27 +1300

    Document `AbstractFunctionStore` and tidy a method signature in it
    Former-commit-id: dd3ebb9f7528f38e63bf235ed1f2641d94f1af66

--- lisien/lisien/types.py
+++ lisien/lisien/types.py
@@ -4513,19 +4513,29 @@ class AbstractStringStore(MutableMapping[str, str], ABC):
 @define
 class AbstractFunctionStore[_K: str, _V: FunctionType | MethodType](ABC):
 	@abstractmethod
-	def save(self, reimport: bool = True) -> None: ...
+	def save(self, reimport: bool = True) -> None:
+		"""Write all my code to the file I represent."""
 
 	@abstractmethod
-	def reimport(self) -> None: ...
+	def reimport(self) -> None:
+		"""Load any changes to the code in my file."""
 
 	@abstractmethod
-	def iterplain(self) -> Iterator[tuple[str, str]]: ...
+	def iterplain(self) -> Iterator[tuple[str, str]]:
+		"""Iterate over pairs of the names of my functions and their code.
+
+		It's 'plain' in the sense that you get a string with the code in it,
+		rather than a function object or a code object.
+
+		"""
 
 	@abstractmethod
-	def store_source(self, v: str, name: _K | None = None) -> None: ...
+	def store_source(self, source: str, name: _K | None = None) -> None:
+		"""Store the source code, ``source``, of a function named ``name``"""
 
 	@abstractmethod
-	def get_source(self, name: _K) -> str: ...
+	def get_source(self, name: _K) -> str:
+		"""Return the source code, in a string, of a function named ``name``"""
 
 
 class StatAlias:
