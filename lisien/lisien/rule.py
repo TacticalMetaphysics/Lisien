a6532490fbcfdf35061b64c89594c66ac5370ef9
Zachary Spector <public@zacharyspector.com>
2026-02-16 16:12:20 +1300

    Rename `RuleBook` to just `Rulebook`
    I don't remember why I camelcased it
    
    
    Former-commit-id: f92e37180c7bc8dab3d2053b503fa2b96bfb888d

--- lisien/lisien/rule.py
+++ lisien/lisien/rule.py
@@ -635,7 +635,7 @@ class Rule:
 		self.triggers.append(self.engine.trigger.truth)
 
 
-class RuleBook(MutableSequence[Rule], Signal):
+class Rulebook(MutableSequence[Rule], Signal):
 	"""A list of rules to be followed for some Character, or a part of it"""
 
 	def _get_cache(
@@ -818,12 +818,12 @@ class RuleMapping(MutableMapping[RuleName, Rule], Signal):
 
 	"""
 
-	rulebook: RuleBook
+	rulebook: Rulebook
 
-	def __init__(self, engine: "Engine", rulebook: RuleBook | RulebookName):
+	def __init__(self, engine: "Engine", rulebook: Rulebook | RulebookName):
 		super().__init__()
 		self.engine = engine
-		if isinstance(rulebook, RuleBook):
+		if isinstance(rulebook, Rulebook):
 			self.rulebook = rulebook
 		else:
 			self.rulebook = self.engine.rulebook[rulebook]
@@ -924,8 +924,8 @@ class RulebookDescriptor:
 			return
 		return instance._get_rulebook()
 
-	def __set__(self, instance: RuleFollower, value: RuleBook | RulebookName):
-		if isinstance(value, RuleBook):
+	def __set__(self, instance: RuleFollower, value: Rulebook | RulebookName):
+		if isinstance(value, Rulebook):
 			instance._set_rulebook_name(value.name)
 		else:
 			instance._set_rulebook_name(value)
@@ -949,7 +949,7 @@ class RuleFollower(ABC):
 
 	rulebook = RulebookDescriptor()
 
-	def _get_rulebook(self) -> RuleBook:
+	def _get_rulebook(self) -> Rulebook:
 		return self.engine.rulebook[self._get_rulebook_name()]
 
 	def rules(self) -> ValuesView[Rule]:
@@ -976,7 +976,7 @@ class RuleFollower(ABC):
 		raise NotImplementedError("_set_rulebook_name")
 
 
-class AllRuleBooks(UserDict[RulebookName, RuleBook], Signal):
+class AllRuleBooks(UserDict[RulebookName, Rulebook], Signal):
 	def __init__(self, engine: "Engine"):
 		Signal.__init__(self)
 		self.engine = engine
@@ -990,24 +990,24 @@ class AllRuleBooks(UserDict[RulebookName, RuleBook], Signal):
 			k, *self.engine.time
 		)
 
-	def __getitem__(self, k: RulebookName | KeyHint) -> RuleBook:
+	def __getitem__(self, k: RulebookName | KeyHint) -> Rulebook:
 		if not isinstance(k, Key):
 			raise TypeError("Invalid rulebook name", k)
 		k = RulebookName(k)
 		if k not in self.data:
-			self.data[k] = RuleBook(self.engine, k)
+			self.data[k] = Rulebook(self.engine, k)
 		return self.data[k]
 
 	def __setitem__(
 		self,
 		key: RulebookName | KeyHint,
-		value: RuleBook | list[Rule | RuleName],
+		value: Rulebook | list[Rule | RuleName],
 	):
 		if not isinstance(key, Key):
 			raise TypeError("Invalid rulebook name", key)
 		key = RulebookName(key)
 		if key not in self.data:
-			self.data[key] = RuleBook(self.engine, key)
+			self.data[key] = Rulebook(self.engine, key)
 		rb = self.data[key]
 		while len(rb) > 0:
 			del rb[0]
