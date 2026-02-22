bc41c99956b488dcab7f851401336af828cb5618
Zachary Spector <public@zacharyspector.com>
2026-02-22 13:19:42 +1300

    Keep the Windows base tarball in git-lfs

--- release.sh
+++ release.sh
@@ -41,7 +41,7 @@ PYTHONPATH=$PWD/lisien:$PWD/elide python -m sphinx . pages/docs/
 if [ ! -d lisien_windows ]; then
   mkdir lisien_windows
 fi
-tar -C lisien_windows -xf pages/lisien-windows.tar.xz
+tar -C lisien_windows -xf lisien-windows.tar.xz
 rm -rf bin lisien/dist elide/dist
 buildozer android clean update debug
 wine lisien_windows/python/python.exe -m pip install --force-reinstall lisien/ elide/ 'parquetdb @ git+https://github.com/lllangWV/ParquetDB.git'
