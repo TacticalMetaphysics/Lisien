2dba5e511af0363c8a58a7f2b8b06c43b9cc21ea
Zachary Spector <public@zacharyspector.com>
2026-03-01 17:03:19 +1300

    Remove version check from make-docs.sh
    That is for deployment

--- make-docs.sh
+++ make-docs.sh
@@ -2,14 +2,10 @@ set -euxo
 python -m sphinx --version
 pyclean --version
 pyclean --debris=all .
-if [ -z "${VERSION+x}" ]; then
-  VERSION=$(python check_version.py)
-fi
 PYTHONPATH=$PWD/lisien:$PWD/elide python -m sphinx html . pages/docs/
 cd pages/docs
 git add .
-git commit -m "Regen docs for v${VERSION}"
-git tag -f "v${VERSION}"
+git commit -m "Regen docs"
 git push ssh://git@codeberg.org/clayote/Lisien.git HEAD:pages
 git push --tags ssh://git@codeberg.org/clayote/Lisien.git HEAD:pages
 cd ../..
