ce12faa71f5a363524a32666268ed8fd39b758f2
Zachary Spector <public@zacharyspector.com>
2026-03-01 16:56:21 +1300

    Fix the core test in required_env.sh

--- required_env.sh
+++ required_env.sh
@@ -7,9 +7,7 @@ required_env_vars=(
   "LISIEN_VERSION"
 );
 for required_env in "${required_env_vars[@]}"; do
-  echo $required_env;
-  echo "${required_env+}"
-  if [ -z "${required_env+}" ]; then
+  if [ -z "${!required_env}" ]; then
     echo "Required environment variable not set: ${required_env}";
     exit 1;
   fi;
