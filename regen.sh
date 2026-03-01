f622a4cebf877fd83730f4c004568dfccad7d73d
Zachary Spector <public@zacharyspector.com>
2026-03-02 09:37:57 +1300

    Generate wheels along with docs

--- regen.sh
+++ regen.sh
@@ -3,9 +3,10 @@ python -m sphinx --version
 pyclean --version
 pyclean --debris=all .
 PYTHONPATH=$PWD/lisien:$PWD/elide python -m sphinx . pages/docs/
-cd pages/docs
+cd pages/
+python -m pip wheel --no-deps ../lisien/ ../elide/
 git add .
-git commit -m "Regen docs"
+git commit -m "Regen docs and wheels"
 git push ssh://git@codeberg.org/clayote/Lisien.git HEAD:pages
 git push --tags ssh://git@codeberg.org/clayote/Lisien.git HEAD:pages
-cd ../..
+cd ../
