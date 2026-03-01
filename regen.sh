fdadc1f9393d2f31752ec8896858678fff82020b
Zachary Spector <public@zacharyspector.com>
2026-03-02 12:45:01 +1300

    Make patched `attrs` pretend to be released `attrs`
    Sphinx seems to require this

--- regen.sh
+++ regen.sh
@@ -1,8 +1,11 @@
 set -euxo
-python -m pip wheel --no-deps git+https://github.com/clayote/attrs.git@sphinx-attr-getter-ext
-python -m pip install attrs-*.whl
-python -m sphinx --version
 pyclean --version
+python -m sphinx --version
+python -m pip install hatch
+git clone --depth 1 -b sphinx-attr-getter-ext https://github.com/clayote/attrs.git
+patch -d attrs -p1 <attrs.patch
+SETUPTOOLS_SCM_PRETEND_VERSION=0.22.4 uv build --wheel attrs/
+python -m pip install attrs-*.whl
 pyclean --debris=all .
 PYTHONPATH=$PWD/lisien:$PWD/elide python -m sphinx . pages/docs/
 cd pages/
